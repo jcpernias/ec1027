2ada6ef4a70ff2530eac4fffd3c6c39c7708e044
José C. Pernías <jcpernias@gmail.com>
2021-03-29 07:00:18 +0200

    Suppress output of code being tested

--- tests/testthat/test-coef_table.R
+++ tests/testthat/test-coef_table.R
@@ -3,24 +3,29 @@ library(sandwich)
 
 mod <- lm(price ~ sqrft + lotsize + bdrms, data = hprice1)
 
+silently <- function(expr) {
+  capture.output({result <- expr})
+  invisible(result)
+}
+
 test_that("coef_table works", {
-  expect_error(coef_table(mod), NA)
+  expect_error(silently(coef_table(mod)), NA)
 })
 
 test_that("coef_table with vce from sandwich packge", {
-  expect_error(coef_table(mod, vce = vcovHC), NA)
+  expect_error(silently(coef_table(mod, vce = vcovHC)), NA)
 })
 
 test_that("coef_table with vce as a string", {
-  expect_error(coef_table(mod, vce = "HC"), NA)
+  expect_error(silently(coef_table(mod, vce = "HC")), NA)
 })
 
 test_that("coef_table with invalid strings for vce", {
-  expect_error(coef_table(mod, vce = c("HC", "HC3")))
-  expect_error(coef_table(mod, vce = "Potato"))
+  expect_error(silently(coef_table(mod, vce = c("HC", "HC3"))))
+  expect_error(silently(coef_table(mod, vce = "Potato")))
 })
 
 test_that("coef_table with invalid vce", {
-  expect_error(coef_table(mod, vce = list(1, 2)))
-  expect_error(coef_table(mod, vce = TRUE))
+  expect_error(silently(coef_table(mod, vce = list(1, 2))))
+  expect_error(silently(coef_table(mod, vce = TRUE)))
 })
