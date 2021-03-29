a4b0f2b79c572ec4c3ebf2dc86f16d4570cc943d
José C. Pernías <jcpernias@gmail.com>
2021-03-29 07:25:10 +0200

    Clean code

--- R/util.R
+++ R/util.R
@@ -1,25 +1,3 @@
-# Functions in package sandwich drop the rows and columns of aliased
-# coefficients. This function adds the corresponding rows and columns
-# and it fills them with NAs.
-patch_vcov <- function(model, bhat = NULL, vce = NULL, ...) {
-  if(is.null(bhat))
-    bhat <- stats::coef(model)
-  if(is.null(vce))
-    vce = stats::vcov
-  Vbhat <- vce(model, ...)
-
-  k <- length(bhat)
-  if (NROW(Vbhat) == k)
-    return(Vbhat)
-
-  V <- matrix(NA_real_, nrow = k, ncol = k)
-  bnames <- names(bhat)
-  dimnames(V) <- list(bnames, bnames)
-  idx <- which(rownames(Vbhat) %in% bnames)
-  V[idx, idx] <- Vbhat
-  return(V)
-}
-
 # Build htest objects
 F_htest <- function(stat, df1, df2) {
   structure(list(statistic = c(F = stat),
@@ -37,15 +15,14 @@ chisq_htest <- function(chisq, df) {
 
 # LM test for heteroskedasticity
 het_lm_test <- function(model, Z, chisq = FALSE) {
-  name <- substitute(model)
   y <- stats::resid(model)^2
   aux <- stats::.lm.fit(Z, y)
 
-  SST <- sum((y-mean(y))^2)
+  SST <- sum((y - mean(y))^2)
   SSR <- sum(stats::resid(aux)^2)
   N <- length(y)
   k <- aux$rank - 1
-  if(chisq) {
+  if (chisq) {
     Rsq <- 1 - SSR / SST
     lm_test <- chisq_htest(N * Rsq, k)
   } else {
@@ -64,14 +41,4 @@ model_call <- function(model) {
   cll[[2]] <- frml
   names(cll)[2] <- ""
   deparse1(cll)
-}
-
-dots_to_str <- function(...) {
-  dots <- substitute(...())
-  if (is.null(dots))
-    return("")
-  str <- deparse1(dots)
-  end <- nchar(str) - 1
-  start <- nchar(class(dots)) + 2
-  substr(deparse1(dots), start, end)
 }
